a62cd1d8fb1313951ed761922e54964e88824393
ThriveTrader <156680023+ThriveTrader@users.noreply.github.com>
2024-01-17 11:27:57 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -16,10 +16,10 @@ const Home = () => {
   return (
     <>
       <Head>
-        <title>Nextly - Free Nextjs & TailwindCSS Landing Page Template</title>
+        <title>Thrive Trader</title>
         <meta
           name="description"
-          content="Nextly is a free landing page template built with next.js & Tailwind CSS"
+          content="Learn Trading with Abhishek"
         />
         <link rel="icon" href="/favicon.ico" />
       </Head>
