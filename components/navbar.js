4392475b0ee2d709cde52967b3b2dde9bd5bee33
ThriveTrader <156680023+ThriveTrader@users.noreply.github.com>
2024-01-17 12:07:47 +0530

    Update navbar.js

--- components/navbar.js
+++ components/navbar.js
@@ -31,7 +31,7 @@ const Navbar = () => {
                         className="w-8"
                       />
                     </span>
-                    <span>Nextly</span>
+                    <span>Thrive Trading</span>
                   </span>
                 </Link>
 
